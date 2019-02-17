e686e153e94acf9edf8d7f5968b04e9ab4f59e3f
KarthikV <kvvetrivel@gmail.com>
2019-02-16 22:49:48 -0800

    -

---
+++ index.js
@@ -0,0 +1,76 @@
+var request = new XMLHttpRequest();
+const APP = document.getElementById('root');
+const DISPLAYED_TEXT = document.getElementById('displayedThought');
+
+request.open('GET', 'https://www.reddit.com/r/showerthoughts.json?limit=500', true);
+request.onload = function () {
+
+
+  var showerThoughtsJSON = JSON.parse(this.responseText);
+
+  if (request.status >= 200 && request.status < 400) {
+    generateRandomThought(showerThoughtsJSON);
+   
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
+
+function generateRandomThought(showerThoughtsJSON) {
+    var listOfThoughts = []
+    showerThoughtsJSON.data.children.forEach(child => {
+      listOfThoughts.push(child.data.title);
+    });
+    listOfThoughts.shift(); // removing the first title.
+    
+    var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
+  
+    while (WordCount(displayedThought) > 20) {
+        var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
+    }
+   
+    DISPLAYED_TEXT.textContent = displayedThought;
+    
+        
+    
+   
+}
+
+
+// Functions for random use
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function WordCount(str) { 
+    return str.split(" ").length;
+  }
+  
+  // Generating the corner time
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
+
+
