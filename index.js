759f7207be6f6f558ed51125ad32a225a5fa01da
KarthikV <kvvetrivel@gmail.com>
2019-02-17 16:51:12 -0800

    Formatting

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ request.open('GET', 'https://www.reddit.com/r/showerthoughts.json?limit=500', tr
 request.onload = function () {
 
 
-  var showerThoughtsJSON = JSON.parse(this.responseText);
+    var showerThoughtsJSON = JSON.parse(this.responseText);
 
-  if (request.status >= 200 && request.status < 400) {
-    generateRandomThought(showerThoughtsJSON);
-   
-  } else {
-    console.log('error');
-  }
+    if (request.status >= 200 && request.status < 400) {
+        generateRandomThought(showerThoughtsJSON);
+
+    } else {
+        console.log('error');
+    }
 }
 
 request.send();
@@ -21,21 +21,15 @@ request.send();
 function generateRandomThought(showerThoughtsJSON) {
     var listOfThoughts = []
     showerThoughtsJSON.data.children.forEach(child => {
-      listOfThoughts.push(child.data.title);
+        listOfThoughts.push(child.data.title);
     });
     listOfThoughts.shift(); // removing the first title.
-    
     var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
-  
     while (WordCount(displayedThought) > 20) {
         var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
     }
-   
     DISPLAYED_TEXT.textContent = displayedThought;
-    
-        
-    
-   
+
 }
 
 
@@ -43,22 +37,22 @@ function generateRandomThought(showerThoughtsJSON) {
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
 
-  function WordCount(str) { 
+function WordCount(str) {
     return str.split(" ").length;
-  }
-  
-  // Generating the corner time
+}
 
-  function checkTime(i) {
+// Generating the corner time
+
+function checkTime(i) {
     if (i < 10) {
-      i = "0" + i;
+        i = "0" + i;
     }
     return i;
-  }
-  
-  function startTime() {
+}
+
+function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -67,14 +61,12 @@ function getRandomInt(max) {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function() {
-      startTime()
+    t = setTimeout(function () {
+        startTime()
     }, 500);
-  }
-  startTime();
+}
+startTime();
 
-  // Setting the background color:
-  var backgroundColors = ["#ACDBC9", "#DBEBC2", "#FDD2B5", "#F7A7A6", "#F48B94, #E5EEC1", "#A2D4AB", "#3EACA8", "#F69A9A", "#F9CDAE", "#C8C8A9", "#83AE9B"]
-  document.body.style.backgroundColor = backgroundColors[getRandomInt(backgroundColors.length)]
-
-
+// Setting the background color:
+var backgroundColors = ["#ACDBC9", "#DBEBC2", "#FDD2B5", "#F7A7A6", "#F48B94, #E5EEC1", "#A2D4AB", "#3EACA8", "#F69A9A", "#F9CDAE", "#C8C8A9", "#83AE9B"]
+document.body.style.backgroundColor = backgroundColors[getRandomInt(backgroundColors.length)]
