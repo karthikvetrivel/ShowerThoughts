aa9f658f9004bd37f44fdc41786fee3a56fccc24
KarthikV <kvvetrivel@gmail.com>
2019-02-18 18:36:58 -0800

    Formatting
    -

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 var request = new XMLHttpRequest();
-const APP = document.getElementById('root');
-const DISPLAYED_TEXT = document.getElementById('displayedThought');
+const APP = document.getElementById("root");
+const DISPLAYED_TEXT = document.getElementById("displayedThought");
 
-request.open('GET', 'https://www.reddit.com/r/showerthoughts.json?limit=500', true);
-request.onload = function () {
-
-
+request.open(
+    "GET",
+    "https://www.reddit.com/r/showerthoughts.json?limit=500",
+    true
+);
+request.onload = function() {
     var showerThoughtsJSON = JSON.parse(this.responseText);
 
     if (request.status >= 200 && request.status < 400) {
         generateRandomThought(showerThoughtsJSON);
-
     } else {
-        console.log('error');
+        console.log("error");
     }
-}
+};
 
 request.send();
 
 function generateRandomThought(showerThoughtsJSON) {
-    var listOfThoughts = []
+    var listOfThoughts = [];
     showerThoughtsJSON.data.children.forEach(child => {
         listOfThoughts.push(child.data.title);
     });
     listOfThoughts.shift(); // removing the first title.
     var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
     while (WordCount(displayedThought) > 20) {
-        var displayedThought = listOfThoughts[getRandomInt(listOfThoughts.length)];
+        var displayedThought =
+            listOfThoughts[getRandomInt(listOfThoughts.length)];
     }
     DISPLAYED_TEXT.textContent = displayedThought;
-
 }
-
 
 // Functions for random use
 
@@ -60,13 +60,26 @@ function startTime() {
     // add a zero in front of numbers<10
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function () {
-        startTime()
+    document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+        startTime();
     }, 500);
 }
 startTime();
 
 // Setting the background color:
-var backgroundColors = ["#ACDBC9", "#DBEBC2", "#FDD2B5", "#F7A7A6", "#F48B94, #E5EEC1", "#A2D4AB", "#3EACA8", "#F69A9A", "#F9CDAE", "#C8C8A9", "#83AE9B"]
-document.body.style.backgroundColor = backgroundColors[getRandomInt(backgroundColors.length)]
+var backgroundColors = [
+    "#ACDBC9",
+    "#DBEBC2",
+    "#FDD2B5",
+    "#F7A7A6",
+    "#F48B94, #E5EEC1",
+    "#A2D4AB",
+    "#3EACA8",
+    "#F69A9A",
+    "#F9CDAE",
+    "#C8C8A9",
+    "#83AE9B"
+];
+document.body.style.backgroundColor =
+    backgroundColors[getRandomInt(backgroundColors.length)];
